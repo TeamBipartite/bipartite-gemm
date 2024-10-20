b1c48b63859ea5e585a12a68510df171ec659564
Johnathan Warawa <johnnyw@johnnyw.ca>
2024-10-20 16:09:59 -0700

    a2: fix typo in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
-#! /binb/bash
+#! /bin/bash
+
+# set up thread count for multithreaded CPU matrix multiply
+#export OPENBLAS_NUM_THREADS=$(nproc)
 
 # For RTX 3060
-nvcc -o a2 main.cu -O3 -g -std=c++20 -arch=sm_86
+nvcc -o a2 main.cu -O3 -g -std=c++20 -arch=sm_86 #-lopenblas
 
 # For Tesla T4
 #nvcc -o a2 main.cu -O3 -g -std=c++20 -arch=sm_75
